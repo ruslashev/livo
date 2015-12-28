8174c67ce58ec869b538ed3cb30e9292cb1f78c7
ruslashev <ruslashev@gmail.com>
2015-12-28 22:19:39 +0300

    move code from gfx.hh to gfx.cc

--- gfx.hh
+++ gfx.hh
@@ -40,103 +40,11 @@ struct atlas {
 
   std::vector<glyph> glyphs;
 
-  atlas(FT_Face face, int height) {
-    faceptr = face;
-    font_height = height;
-    glyphs.resize(1, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
-
-    add_new_texture();
-
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-    query('\0');
-  }
-
-  const glyph* query(unsigned int codepoint) {
-    // printf("query glyph %c\n", codepoint);
-    if (codepoint >= glyphs.size()) {
-      glyphs.resize(codepoint+1, { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
-      // for (size_t i = 0; i < glyphs.size(); i++)
-      // 	glyphs
-    }
-    if (!glyphs[codepoint].rendered)
-      render_char(codepoint);
-    return &glyphs[codepoint];
-  }
-
-  void add_new_texture() {
-    puts("add_new_texture()");
-    GLuint texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_ALPHA, MAXWIDTH, MAXWIDTH, 0,
-        GL_ALPHA, GL_UNSIGNED_BYTE, 0);
-
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    textures.push_back(texture);
-
-    texture_last_x = 0;
-    texture_last_y = 0;
-    row_height = 0;
-  }
-
-  void render_char(unsigned int i) {
-    FT_Set_Pixel_Sizes(faceptr, 0, font_height);
-    FT_GlyphSlot g = faceptr->glyph;
-
-    if (FT_Load_Char(faceptr, i, FT_LOAD_RENDER)) {
-      fprintf(stderr, "Loading character '%c' failed!\n", i);
-      return;
-    }
-
-    if (texture_last_x + g->bitmap.width + 1 >= MAXWIDTH) {
-      if (texture_last_y + row_height >= MAXWIDTH) {
-        add_new_texture();
-        row_height = 0;
-      } else {
-        texture_last_y += row_height;
-        row_height = 0;
-        texture_last_x = 0;
-      }
-    }
-
-    printf("texture %d access\n", textures.back());
-    glyphs[i].texture = textures.back();
-
-    glyphs[i].rendered = true;
-
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, textures.back());
-
-    glTexSubImage2D(GL_TEXTURE_2D, 0, texture_last_x, texture_last_y,
-        g->bitmap.width, g->bitmap.rows, GL_ALPHA, GL_UNSIGNED_BYTE,
-        g->bitmap.buffer);
-
-    glyphs[i].advance_x = g->advance.x >> 6;
-    glyphs[i].advance_y = g->advance.y >> 6;
-
-    glyphs[i].bitmap_w = g->bitmap.width;
-    glyphs[i].bitmap_h = g->bitmap.rows;
-
-    glyphs[i].bitmap_left = g->bitmap_left;
-    glyphs[i].bitmap_top = g->bitmap_top;
-
-    glyphs[i].texture_offset_x = texture_last_x / (float)MAXWIDTH;
-    glyphs[i].texture_offset_y = texture_last_y / (float)MAXWIDTH;
-
-    row_height = std::max(row_height, g->bitmap.rows);
-    texture_last_x += g->bitmap.width + 1;
-  }
-
-  ~atlas() {
-    for (size_t i = 0; i < textures.size(); i++)
-      glDeleteTextures(1, &textures[i]);
-  }
+  atlas(FT_Face face, int height);
+  ~atlas();
+  const glyph* query(unsigned int codepoint);
+  void add_new_texture();
+  void render_char(unsigned int i);
 };
 
 class gfx {
